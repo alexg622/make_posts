840a408257bc6f0be75017dfc82b8c0f59293c0a
alexg622 <alex.gm62288@gmail.com>
2018-07-17 21:55:00 -0700

    add gitignore connect database to server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://alexg622:Smartwat3r@ds141661.mlab.com:41661/make_posts"
+}
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const users = require('./routes/api/users')
 const posts = require('./routes/api/posts')
+const db = require('./config/keys').mongoURI
+
+mongoose.connect(db).then(() => console.log('mongoDB connected')).catch(err => console.log(err))
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
