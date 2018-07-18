7b3e4a5d1b7d354bd3af68129ff9c7ea6376198a
alexg622 <alex.gm62288@gmail.com>
2018-07-17 21:49:26 -0700

    Make user and post routes

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const users = require('./routes/api/users')
+const posts = require('./routes/api/posts')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.get('/test', (req, res) => res.send("I work"))
+
+app.use('/api/users', users)
+app.use('/api/posts', posts)
 
 const port = process.env.PORT || 5000
 
